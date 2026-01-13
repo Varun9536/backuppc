0e7004c701bc84d6cf7168e82c21d334075188f6
varun <varunkumar956685@gmail.com>
2026-01-13 18:24:03 +0530

    update

--- src/services/api.js
+++ src/services/api.js
@@ -5,10 +5,10 @@
 const BASE_URL = '';
 const BASE_URL_CLOUD = "https://renewal-situated-supplement-elevation.trycloudflare.com"//"http://127.0.0.1:8082";
 const BASE_URL_LOG = "https://improve-produced-auto-absolute.trycloudflare.com"//"http://127.0.0.1:8085";
-const BASE_URL_READLOG = "https://sin-pre-oregon-massachusetts.trycloudflare.com"//"http://127.0.0.1:3000";
+const BASE_URL_READLOG = "https://income-touched-directory-partners.trycloudflare.com"//"http://127.0.0.1:3000";
 const BASE_URL_PERMISSION = "https://series-services-catalog-conservation.trycloudflare.com"//"http://127.0.0.1:8084";
 const BASE_URL_SETTING = "https://cpu-tagged-myrtle-strain.trycloudflare.com"//"http://127.0.0.1:8088";
-const BASE_URL_SCHEDULE = "https://privileges-daisy-valuation-dec.trycloudflare.com"//"http://127.0.0.1:5000";
+const BASE_URL_SCHEDULE = "https://mysimon-brakes-zen-registry.trycloudflare.com"//"http://127.0.0.1:5000";
 const BASE_URL_UPDATE = "https://history-realm-enforcement-notebook.trycloudflare.com/api/providers"//"http://127.0.0.1:5001/api/providers";
 const BASE_URL_GET_SCHEDULE = "https://cope-shelter-preferences-confidence.trycloudflare.com/"//"http://127.0.0.1:5002/";
 
