3270c29dcbfea869ea5de90725cfa56045f3dac4
varun <varunkumar956685@gmail.com>
2026-01-13 17:13:02 +0530

    update cloudfare urls

--- src/services/api.js
+++ src/services/api.js
@@ -4,13 +4,13 @@
 
 const BASE_URL = '';
 const BASE_URL_CLOUD = "https://renewal-situated-supplement-elevation.trycloudflare.com"//"http://127.0.0.1:8082";
-const BASE_URL_LOG = "https://bridges-packages-prices-ottawa.trycloudflare.com"//"http://127.0.0.1:8085";
-const BASE_URL_READLOG = "https://beef-printer-dealing-die.trycloudflare.com"//"http://127.0.0.1:3000";
-const BASE_URL_PERMISSION = "https://cause-merge-vcr-sticker.trycloudflare.com"//"http://127.0.0.1:8084";
-const BASE_URL_SETTING = "https://knights-lined-herself-tom.trycloudflare.com"//"http://127.0.0.1:8088";
-const BASE_URL_SCHEDULE = "https://new-floating-accepting-title.trycloudflare.com"//"http://127.0.0.1:5000";
-const BASE_URL_UPDATE = "https://hire-oklahoma-thermal-antibody.trycloudflare.com/api/providers"//"http://127.0.0.1:5001/api/providers";
-const BASE_URL_GET_SCHEDULE = "https://fees-perceived-shipments-colleagues.trycloudflare.com"//"http://127.0.0.1:5002/";
+const BASE_URL_LOG = "https://improve-produced-auto-absolute.trycloudflare.com"//"http://127.0.0.1:8085";
+const BASE_URL_READLOG = "https://sin-pre-oregon-massachusetts.trycloudflare.com"//"http://127.0.0.1:3000";
+const BASE_URL_PERMISSION = "https://series-services-catalog-conservation.trycloudflare.com"//"http://127.0.0.1:8084";
+const BASE_URL_SETTING = "https://cpu-tagged-myrtle-strain.trycloudflare.com"//"http://127.0.0.1:8088";
+const BASE_URL_SCHEDULE = "https://privileges-daisy-valuation-dec.trycloudflare.com"//"http://127.0.0.1:5000";
+const BASE_URL_UPDATE = "https://history-realm-enforcement-notebook.trycloudflare.com/api/providers"//"http://127.0.0.1:5001/api/providers";
+const BASE_URL_GET_SCHEDULE = "https://fotos-olive-screening-end.trycloudflare.com"//"http://127.0.0.1:5002/";
 
 // Utility delay function
 const delay = (ms = 300) => new Promise(resolve => setTimeout(resolve, ms));
